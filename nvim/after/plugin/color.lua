c2cb59301e421d2a29551a2f2e13a9f279e5756d
Christian Genderka <christian@genderka.email>
2023-03-13 00:03:00 +0100

    italic disabled in themes

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -1,17 +1,28 @@
 require("catppuccin").setup({
   transparent_background = false,
+  no_italic = true,
+  styles = {
+    functions = { "bold" },
+  },
 });
--- vim.cmd("colorscheme catppuccin")
-
+vim.cmd("colorscheme catppuccin")
 -- vim.cmd [[colorscheme nightfly]]
 
--- require("rose-pine").setup({
---   disable_background = false,
---   groups = {
---     background = 'base'
---   }
--- });
-vim.cmd [[colorscheme rose-pine]]
+require("rose-pine").setup({
+  highlight_groups = {
+--    ["@comment"] = { fg = p.muted, italic = false },
+--    ["@variable"] = { fg = p.text, italic = false },
+--    ["@variable.builtin"] = { fg = p.love, italic = false },
+--    ["@tag.attribute"] = { italic = false },
+    ["Function"] = { fg = p.rose, bold = true },
+  },
+--  disable_background = false,
+--  groups = {
+--    background = 'base'
+--  },
+  disable_italics = true,
+});
+
 
 local hl = function(thing, opts)
   vim.api.nvim_set_hl(0, thing, opts)
@@ -21,20 +32,4 @@ hl("LineNr", {
   fg = "#5eacd3"
   -- fg = "#f5f553"
   -- bold=true
-})
-
--- vim.opt.cursorline=true
--- hl("CursorLine", {
---   bold=true
--- })
-
-hl("@function", {
-  bold = true,
-  -- fg = "#e79c9b"
-  -- fg = "#55ffff"
-})
-
-hl("@function.macro", {
-  bold = true,
-  fg = "#e79c9b"
 })
